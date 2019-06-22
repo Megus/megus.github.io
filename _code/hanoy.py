bb270c014bc6a134e7dcd221da8e8f7c145f71c9
Roman Petrov <megus.sugem@gmail.com>
2019-06-22 08:29:38 +0300

    Working on non-recursive Tower of Hanoi solution for the new blog post

---
+++ _code/hanoy.py
@@ -0,0 +1,10 @@
+def hanoy_recursive(disks, src, dst, hlp):
+  if disks == 1:
+    print(src, dst)
+    return
+
+  hanoy_recursive(disks - 1, src, hlp, dst)
+  print(src, dst)
+  hanoy_recursive(disks - 1, hlp, dst, src)
+
+hanoy_recursive(6, 1, 3, 2)
